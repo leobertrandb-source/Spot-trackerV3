5998d14aa9c0acb164c4acd42cc0afa30bf862dd
leobertrandb-source <leo.bertrand.b@gmail.com>
2026-03-02 22:19:51 +0100

    Update2Sidebar.jsx

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -194,18 +194,11 @@ export default function Sidebar() {
           <span style={{ fontSize: 12 }}>→</span> Déconnexion
         </button>
         
+        <div style={{position:"fixed",bottom:10,right:10,background:"yellow",padding:10,zIndex:9999}}>
+      DESIGN TEST
+    </div>
+        
 </div>
-      </div><div
-  style={{
-    position: "fixed",
-    bottom: 10,
-    right: 10,
-    background: "yellow",
-    padding: 10,
-    zIndex: 9999,
-  }}
->
-  DESIGN TEST
 </div>
     </aside>
   )
