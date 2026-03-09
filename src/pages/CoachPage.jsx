1e87fbf43b0ddab609220f9a9df657c98701429d
leobertrandb-source <leo.bertrand.b@gmail.com>
2026-03-09 22:26:47 +0100

    Update CoachPage.jsx

--- src/pages/CoachPage.jsx
+++ src/pages/CoachPage.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 import { useEffect, useState } from 'react'
-import { AuthProvider, useAuth } from './components/AuthContext'
+import { AuthProvider, useAuth } from '../components/AuthContext'
 import { Grain, Layout } from './components/UI'
 import { DirtyProvider } from './components/DirtyContext'
 import Sidebar from './components/Sidebar'
